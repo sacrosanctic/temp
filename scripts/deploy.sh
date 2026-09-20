5b9a5c477a8afa864c66a50a50e5228952a2b3b6
Scott Wu <sw@scottwu.ca>
2026-09-20 21:23:36 +0800

    deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 git pull
 
-BUILD_VERSION=$(git rev-parse HEAD)
+export BUILD_VERSION=$(git rev-parse HEAD)
 
 echo "$(date --utc +%FT%TZ): Releasing new server version. $BUILD_VERSION"
 
@@ -13,13 +13,13 @@ docker compose build
 
 OLD_CONTAINER=$(docker ps -aqf "name=server")
 echo "$(date --utc +%FT%TZ): Scaling server up..."
-BUILD_VERSION=$BUILD_VERSION docker compose up -d --no-deps --scale server=2 --no-recreate server
+docker compose up -d --no-deps --scale server=2 --no-recreate server
 
 sleep 30
 
 echo "$(date --utc +%FT%TZ): Scaling old server down..."
 docker container rm -f $OLD_CONTAINER
-docker compose up -d --no-deps --scale server=1 --no-create server
+docker compose up -d --no-deps --scale server=1 --no-recreate server
 
 echo "$(date --utc +%FT%TZ): Running build..."
 CADDY_CONTAINER=$(docker ps -aqf "name=caddy")
